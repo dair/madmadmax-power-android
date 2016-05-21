30e54a7abee9e4a7ae83ebb57233f259365834d3
Vladimir Lebedev-Shmidtgof <dair@joybits.org>
2016-05-21 22:48:14 +0300

    Adding BluetoothDeviceActivity

---
+++ app/src/main/java/org/albiongames/madmadmax/power/BluetoothDeviceActivity.java
@@ -0,0 +1,14 @@
+package org.albiongames.madmadmax.power;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BluetoothDeviceActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_device);
+    }
+}
